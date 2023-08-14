e960f941289e86a7ded03df4dab9443eee698011
DarinHajou <it_related@hotmail.com>
2023-08-14 13:43:26 +0200

    Make the gallery responsive

--- _app/js/modules/displayTransformedImages.js
+++ _app/js/modules/displayTransformedImages.js
@@ -48,7 +48,7 @@ export default async function displayTransformedImages() {
    */
   function createTransformedImageCard(image) {
     const card = document.createElement('div');
-    card.classList.add('card', 'grid__column--3');
+    card.classList.add('card');
 
     const imageLink = document.createElement('a');
 
